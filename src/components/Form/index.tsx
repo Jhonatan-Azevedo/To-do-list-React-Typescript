2aa621e568b069144f9b6cdcfadf276088d2ec6f
Jhonatan-Azevedo <dev.azevedo@outlook.com>
2022-08-26 16:02:10 -0300

    ADD: get assigment and timerAssigment

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -3,16 +3,40 @@ import Button from "../Button";
 import style from "./Form.module.scss"
 
 class Form extends React.Component {
+    state = {
+        assignment: "",
+        timeAssingment: "00:00:00"
+    }
+
+    saveAssigment(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        console.log("state: ", this.state);
+    }
+
     render() {
         return (
-            <form className={style.novaTarefa}>
+            <form className={style.novaTarefa} onSubmit={this.saveAssigment.bind(this)}>
                 <div className={style.inputContainer}>
                     <label htmlFor="assignment">Tarefa: </label>
-                    <input type="text" name="assignment" id="assignment" placeholder="Estudar" required />
+                    <input
+                        type="text"
+                        name="assignment"
+                        id="assignment"
+                        value={this.state.assignment}
+                           onChange={(event) => this.setState({ ... this.state, assignment: event.target.value })}
+                        placeholder="Estudar"
+                        required />
                 </div>
                 <div className={style.inputContainer}>
                     <label htmlFor="timeAssingment">Tempo: </label>
-                    <input type="time" name="timeAssingment" id="timeAssingment" placeholder="00:00:00" />
+                    <input
+                        type="time"
+                        name="timeAssingment"
+                        id="timeAssingment"
+                        value={this.state.timeAssingment}
+                        onChange={(event) => this.setState({ ... this.state, timeAssingment: event.target.value })}
+                        placeholder="00:00:00"
+                        required />
                 </div>
                 <Button>
                     Adicionar
